1ecb57f4c8f905c5d33067e54f851744426088f8
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-01 21:15:44 -0800

    Text input field created and wired up

--- grapheme/client/grapheme.js
+++ grapheme/client/grapheme.js
@@ -1,19 +1,7 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to grapheme.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Template.content.events({
+  'click input' : function (e) {
+    // submit user input data for parsing
+    var string = document.getElementById("userstring").value;
+    // Meteor.call('parsePOS', string)
+  }
+});
