337208cff7d42d5cf33a642286fe148c3b7917cc
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-02 14:44:04 -0800

    Removing old extraneous files before merge

--- grapheme/client/grapheme.js
+++
@@ -1,7 +0,0 @@
-Template.content.events({
-  'click input' : function (e) {
-    // submit user input data for parsing
-    var string = document.getElementById("userstring").value;
-    // Meteor.call('parsePOS', string)
-  }
-});
