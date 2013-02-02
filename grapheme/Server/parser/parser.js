335b20dddf7e78b37e2b2caa2d37a5262e0d2cb8
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-01 22:30:31 -0800

    Fixed crashing bug in parser.js

--- grapheme/Server/parser/parser.js
+++ grapheme/Server/parser/parser.js
@@ -8,17 +8,5 @@ Meteor.methods({
     var words = new Lexer().lex(prePOS);
     var taggedWords = new POSTagger().tag(words);
     LiveCode.update({phrase: /.+/}, {phrase: taggedWords});
-    };
-  },
-
-  PearsonAPI: function (word, POS) {
-    // TODO build dictionary call method.
-    // for (i in taggedWords) {
-    //   var taggedWord = taggedWords[i];
-    //   var word = taggedWord[0];
-    //   var tag = taggedWord[1];
-    //   // TODO test if word in dictionary.
-    //   // TODO figure out what to return to client.
-    //   console.log(word + " /" + tag);
   }
 });
