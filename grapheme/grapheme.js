6db197d384b7cae327bde11568c118507e5cbc6c
Albert Lee <Monsterblader@gmail.com>
2013-02-01 21:59:49 -0800

    parsePOS should be working.  Untested.  I think that the order of the files in pos-js should be fine.

--- grapheme/grapheme.js
+++ grapheme/grapheme.js
@@ -17,17 +17,24 @@ if (Meteor.isServer) {
     parsePOS: function (prePOS) {
       var words = new Lexer().lex(prePOS);
       var taggedWords = new POSTagger().tag(words);
-      for (i in taggedWords) {
-        var taggedWord = taggedWords[i];
-        var word = taggedWord[0];
-        var tag = taggedWord[1];
-        // TODO figure out what to return to client.
-        console.log(word + " /" + tag);
+      LiveCode.update({phrase: /.+/}, {phrase: taggedWords});
       };
+    },
+
+    PearsonAPI: function (word, POS) {
+      // TODO build dictionary call method.
+      // for (i in taggedWords) {
+      //   var taggedWord = taggedWords[i];
+      //   var word = taggedWord[0];
+      //   var tag = taggedWord[1];
+      //   // TODO test if word in dictionary.
+      //   // TODO figure out what to return to client.
+      //   console.log(word + " /" + tag);
     }
   });
 
   Meteor.startup(function () {
+    LastPhrase.insert({phrase: "nothing"});
     // code to run on server at startup
   });
 }
