0b1a8113720d781730a7d3ae6e2b85dbe1ab321d
Mark Reveley <mreveley@gmail.com>
2013-02-02 12:38:19 -0800

    began refactor to build definitions array on server

--- grapheme/Server/longman-api-call.js
+++ grapheme/Server/longman-api-call.js
@@ -1,9 +1,16 @@
+var definitonsArray = [];
+
 if (Meteor.isServer) { // rev note: without this if, throws following error even though code is within the server folder: "Error {} Error: Can't make a blocking HTTP call from the client; callback required.""
 	Meteor.methods({
 	    longmanAPICall: function (word) {
 	        this.unblock();
 	        var url = "https://api.pearson.com/longman/dictionary/entry.json?q=" + word + "&apikey=2884334e050d5be2e5f001d00b4f1d28";
-	        return Meteor.http.call("GET", url);
+	        def = Meteor.http.call("GET", url);
+	        definitonsArray.push(def);
 	    }
 	});
 }
+
+
+// console.log(definitonsArray);
+
--- grapheme/client/main.js
+++ grapheme/client/main.js
@@ -6,16 +6,24 @@ Template.content.events({
   }
 });
 
+
 var sentence = "The quick brown";
+sentence = sentence.toLowerCase();
 var sentenceArray = sentence.split(" ");
+// TO IMPLEMENT
+// function checkSpelling() {
+// 	// check that a word is a valid word with valid spelling via a local spellchecker which will save on api calls
+// }
+// checkspelling() // either within a seperate for loop, or more likely inserted into the loop below
 
 for (var i = 0; i <= sentenceArray.length; i++) {
 	Meteor.call("longmanAPICall", sentenceArray[i], function(error, results) {
 			var entry = results.content;
-	    console.log(entry); 
+	    // console.log(entry); 
 	    return entry;
 	});
 }
+
 // var word = "after";
 
 
