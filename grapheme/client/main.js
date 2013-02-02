538ea2ec3bfcfc763942109679c0026f5af48644
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-02 14:16:24 -0800

    Template and helper created to render parsed array as colored text

--- grapheme/client/main.js
+++ grapheme/client/main.js
@@ -2,6 +2,13 @@ Template.content.events({
   'click input' : function (e) {
     // submit user input data for parsing
     var string = document.getElementById("userstring").value;
-    // Meteor.call('parsePOS', string)
+    // var parsedString = Meteor.call('parsePOS', string);
+    // Session.set('parseResults', parsedString);
   }
 });
+
+Template.content.parsedArray = function () {
+  // var parseRes = Session.get('parseResults');
+  // return parseRes
+  return [['Apples', 'NN'],['bananas', 'NN'],['and', 'CC'],['pears', 'NN']]
+}
