bd1a1060b9f71478c26510d4bf058f89076d77ce
JP Whitaker <jpw@hawaii.edu>
2013-02-02 20:31:24 -0800

    clicking on words sets the session variable to that word and displays it on screen

--- grapheme/client/dictionary.js
+++ grapheme/client/dictionary.js
@@ -7,11 +7,24 @@ var wordExistsInUserDict = function(word){
 }
 
 
+Template.content.events({
+  'click span': function (event) {
+    var paragraph = event.currentTarget; // always a P
+    console.log(event.srcElement.innerHTML)
+    var clickedElement = event.target; // could be the P or a child element
+    Session.set('currentWord', event.srcElement.innerHTML)
+  }
+})
 
 
-// Template.dictionary.word = function(){
-//   return after.Entries.Entry[0].Head.HWD["#text"];
-// }
+
+Template.dictionary.word = function(){
+  var returnWord = Session.get('currentWord')
+  return returnWord
+  // if(returnWord.entry.Entries.Entry[0].Head.HWD["#text"] != undefined){
+    // return returnWord.entry.Entries.Entry[0].Head.HWD["#text"];
+    // } 
+}
 
 //   Template.dictionary.partOfSpeech = function(){
 //   return ;
