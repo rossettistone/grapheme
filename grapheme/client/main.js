d383ea33edd58ce5e221059e7e096fc1be766525
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-06 04:24:00 -0800

    Fixed analytics implementation

--- grapheme/client/main.js
+++ grapheme/client/main.js
@@ -35,8 +35,20 @@ Template.content.hasUserInput = function () {
   return Session.get('parsedWords');
 };
 
-Template.content.partOfSpeech = function(){
+Template.content.partOfSpeech = function () {
   var returnPOS = Session.get('currentPOS');
   returnPOS = posToWord(returnPOS);
   return returnPOS;
+};
+
+Template.analytics.rendered = function () {
+  window._gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-38251628-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 };
